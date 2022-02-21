3dc05f88d6dbd9cb759335efdcb5001970a46078
Balajisridhar3108 <92989836+Balajisridhar3108@users.noreply.github.com>
2022-02-21 20:55:32 +0530

    Add files via upload

---
+++ college.java
@@ -0,0 +1,20 @@
+package org.college;
+
+import org.department.department;
+
+public class college extends department{
+	public  static void collegeName() {
+		System.out.println("college name");
+	}
+		public static void collegeCode() {
+			System.out.println("college code");
+		}
+			public static void collegeRank() {
+				System.out.println("college rank");
+				
+			
+		
+	}
+	
+
+}
