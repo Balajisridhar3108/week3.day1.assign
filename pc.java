9980752af1dda9bbfb3f2fd2b479375f00e20b11
Balajisridhar3108 <92989836+Balajisridhar3108@users.noreply.github.com>
2022-02-21 20:53:25 +0530

    Add files via upload

---
+++ pc.java
@@ -0,0 +1,15 @@
+package week3.day1;
+
+public class pc extends laptop {
+	public void getSysteminfo() {
+		System.out.println("pc");
+		
+	}
+		public static void main(String[] args) {
+			pc ps= new pc();
+			ps.getSysteminfo();
+			
+			}
+	}
+
+
